69278b89df2a3deee01e8300f4e1039f002fc2ab
InfinityCent <pereeia@gmail.com>
2024-08-14 17:53:09 -0400

    Added my solution for 03_reverseString and ran all tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const splitString = string.split("");
+    const reverseArray = splitString.reverse();
+    const joinArray = reverseArray.join("")
+    
+    return joinArray;
 };
 
 // Do not edit below this line
