2b0551599d94b8c78b0f4605c1b57c06e5c8ac73
InfinityCent <pereeia@gmail.com>
2024-08-16 17:58:48 -0400

    Added my solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return("ERROR")
+    };
+    
+    if (a > b) {
+        [a, b] = [b, a];
+    };
+
+    let sum = 0;
+    for (i = a; i <= b; i++) {
+        sum += i
+    };
+
+return(sum)
 
 };
 
