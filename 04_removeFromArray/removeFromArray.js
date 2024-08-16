05c5bbb8d8acd43f53f6ecf451b6c3b327f603fa
InfinityCent <pereeia@gmail.com>
2024-08-16 17:28:31 -0400

    Added my own solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...otherArgs) {
+    let index = -1;
+    for (const arg of otherArgs) {
+        index = array.findIndex(x => x === arg);
+        while (index !== -1) {
+            array.splice(index, 1)
+            index = array.findIndex(x => x === arg)
+        };
+    };
+    return(array);
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
