41c1378e6088bf87735424f82602353c909fdfa5
InfinityCent <pereeia@gmail.com>
2024-09-16 17:42:48 -0400

    Added my solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    
+    let initFib = [1, 1]
+    i = 3
+    while (i <= +position) {
+        initFib.push(initFib[initFib.length - 1] + initFib[initFib.length - 2])
+        i++
+    }
 
+    if (+position === 0) {
+        return 0
+    } else if (+position < 0) {
+        return "OOPS"
+    } else {
+        return initFib[initFib.length - 1]
+    }
 };
 
 // Do not edit below this line
