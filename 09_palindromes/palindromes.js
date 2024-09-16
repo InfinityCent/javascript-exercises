69b3b1fafd6eb3ff8ba39ca05b731ee12aad350e
InfinityCent <pereeia@gmail.com>
2024-09-16 17:26:59 -0400

    Added my solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (myStr) {
+    myArr  = myStr.split('')
 
+    function isPunc(letter) {
+        const alphanum = 'abcdefghijklmnopqrstuvwxyz0123456789'.split('')
+        return alphanum.includes(letter.toLowerCase())
+    }
+    
+    noPuncArray = myArr.filter(isPunc)
+
+    caseInsensitiveArr = noPuncArray.map(letter => letter.toLowerCase())
+
+    return caseInsensitiveArr.join("") === caseInsensitiveArr.reverse().join("")
 };
 
 // Do not edit below this line
