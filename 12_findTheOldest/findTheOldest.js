36ecf371aa4992ea4f9a39b2783ea66b78920fe6
InfinityCent <pereeia@gmail.com>
2024-09-16 18:20:09 -0400

    Added my solution for 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(personArr) {
+    
+    calculateAge = function(person) {
+        if (!("yearOfDeath" in person)) {
+            person["yearOfDeath"] = new Date().getFullYear()
+        }
+        person["age"] = person.yearOfDeath - person.yearOfBirth
+        return person
+    }
 
+    ageArr = personArr.map(calculateAge)
+    highestAge = Math.max(...ageArr.map((p) => p.age))
+    return ageArr.filter((p) => p.age == highestAge)[0]
 };
 
 // Do not edit below this line
