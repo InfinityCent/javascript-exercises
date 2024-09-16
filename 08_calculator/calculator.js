4da73987e03f6d34b7cd42a7cdd21c47b08d5838
InfinityCent <pereeia@gmail.com>
2024-09-16 16:54:37 -0400

    Added my solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return(a + b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return(a - b)
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return(arr.reduce((arrSum, a) => arrSum + a, 0))
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return(arr.reduce((arrMulti, a) => arrMulti * a, 1))
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return(a ** b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	arr = Array(a).fill().map((_, index) => index + 1)
+
+  return(arr.reduce((arrMulti, a) => arrMulti * a, 1))
 };
 
 // Do not edit below this line
